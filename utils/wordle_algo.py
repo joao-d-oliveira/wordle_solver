487c8be6280e73374e1c6c93439e887675fea880
Joao Oliveira <joao.d.oliveira@gmail.com>
2022-03-04 20:30:46 -0300

    moved into utils folder

--- utils/wordle_algo.py
+++ utils/wordle_algo.py
@@ -1,7 +1,7 @@
 def solve_worldle(date_of_challenge):
     import datetime
 
-    Ma = open('/Users/joao/Documents/Python/wordleSolver/corpus/wordle_av.txt', 'r').read().split('\n')
+    Ma = open('/corpus/wordle_av.txt', 'r').read().split('\n')
     Ga = datetime.datetime(2021, 6, 19, 0, 0, 0, 0).replace(hour=0, minute=0, second=0, microsecond=0)
     date_of_challenge_ = date_of_challenge.replace(hour=0, minute=0, second=0, microsecond=0)
 
